4a48a2fe1062c96d30dddbe445978fbfa2e965e6
Michael Cantrell <micantre@calpoly.edu>
2018-09-27 13:10:37 -0700

    Added Files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameController : MonoBehaviour {
+
+    public Tilemap tilemap;
+    public TileBase wall;
+    GameObject go;
+
+    // Use this for initialization
+    void Start ()
+    {
+        go = new GameObject();
+        Tile t = ScriptableObject.CreateInstance<Tile>();
+        t.gameObject = go;
+        t.color = Color.red;
+        tilemap.SetTile(new Vector3Int(0, 0, 0), t);
+        tilemap.SetTile(new Vector3Int(0, 2, 0), t);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rigidBody;
+	public float walkSpeed = 5f;
+
+	void FixedUpdate() {
+		rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * walkSpeed, Input.GetAxis("Vertical") * walkSpeed);
+	}
+}
